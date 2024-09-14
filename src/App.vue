51c556917d973db2fba83c83f47216f298000ca4
FeranmiDev <macguzik98@gmail.com>
2024-09-14 12:59:00 +0100

    Refactor code to warm up server on app startup

--- src/App.vue
+++ src/App.vue
@@ -8,13 +8,16 @@
 <script setup>
 	import { computed } from 'vue';
 	import { useRoute } from 'vue-router';
-	import OrganizationHeader from './components/OrganizationHeader.vue';
-
+	import { warmUp } from './utils/misc';
+	import OrganizationHeader from './components/OrganizationHeader.vue'
 	const route = useRoute();
-
+	
+	warmUp();
 	const layout = computed(() => {
 		return route.meta.layout || 'div';
 	});
 </script>
+
+
 
 <style scoped></style>
---
+++ src/utils/misc.js
@@ -0,0 +1,13 @@
+import apiClient from "../apiConfig";
+
+// warm up the server
+
+export const warmUp = async () => {
+    try {
+        const response = await apiClient.get("/api/v1/warm-up");
+        return response.data;
+    } catch (error) {
+        console.error("Warm up error:", error);
+        throw error;
+    }
+}
